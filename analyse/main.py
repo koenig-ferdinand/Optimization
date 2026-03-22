27db7b1a8d4ba7b2e488f3ac4a507d7f3a23b762
Ferdinand König <ferdinand.koenig@outlook.com>
2026-03-22 14:54:33 +0100

    comparision

--- analyse/main.py
+++ analyse/main.py
@@ -10,34 +10,41 @@ import functions
 
 # MAIN.PY
 # read from weightspace 
-data = torch.load('data/state_step000100.pt', map_location = 'cpu')
+data = torch.load('data/muon/state_step006200.pt', map_location = 'cpu')
 model = data['model']
 
 # ITERATE over matrices
-#for name, matrix in model.items(): 
+# for name, matrix in model.items(): 
 #    print(f"{name:45s} {tuple(matrix.shape)}")
 
 # ACCESS matrices, where hidden layers range: 0-11
-# model['transformer.h.0.attn.c_attn.weight'] # layer 0, attention QKV (3*768, 768)
-# model['transformer.h.0.attn.c_proj.weight'] # layer 0, attention output
-# model['transformer.h.0.mlp.c_fc.weight'] # layer 0, MLP expand
-# model['transformer.h.0.mlp.c_proj.weight'] # layer 0, MLP compress
+# model['_orig_mod.transformer.h.0.attn.c_attn.weight'] # layer 0, attention QKV (3*768, 768)
+# model['_orig_mod.transformer.h.0.attn.c_proj.weight'] # layer 0, attention output
+# model['_orig_mod.transformer.h.0.mlp.c_fc.weight'] # layer 0, MLP expand
+# model['_orig_mod.transformer.h.0.mlp.c_proj.weight'] # layer 0, MLP compress
 
 # SVD 
-S = functions.svd(model['transformer.h.11.attn.c_proj.weight'])
-print(f"\n SINGULAR VALUES: \n {S}")
+layers = ['_orig_mod.transformer.h.11.attn.c_attn.weight', 
+          '_orig_mod.transformer.h.11.attn.c_proj.weight', 
+          '_orig_mod.transformer.h.11.mlp.c_fc.weight', 
+          '_orig_mod.transformer.h.11.mlp.c_proj.weight']
 
-# STABLE RANK
-# stable_rank = functions.stable_rank(S)
-# print(f"STABLE RANK: {stable_rank}")
+for layer in layers: 
+    S = functions.svd(model[layer])
+    print(f"\nLAYER: {layer}")
+    # print(f"SINGULAR VALUES: \n {S}")
 
-# EFFECTIVE RANK
-effective_rank = functions.effective_rank(S)
-print(f"EFFECTIVE RANK: {effective_rank}")
+    # STABLE RANK
+    stable_rank = functions.stable_rank(S)
+    print(f"STABLE RANK: {stable_rank}")
 
-# CONDITION NUMBER
-# cond_num = functions.condition_number(S)
-# print(f"CONDITION NUMBER: {cond_num}")
+    # EFFECTIVE RANK
+    effective_rank = functions.effective_rank(S)
+    print(f"EFFECTIVE RANK: {effective_rank}")
+
+    # CONDITION NUMBER
+    cond_num = functions.condition_number(S)
+    print(f"CONDITION NUMBER: {cond_num}")
 
 
 
